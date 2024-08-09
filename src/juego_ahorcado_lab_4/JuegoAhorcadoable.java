27d6083b497a36bdf4782da9b7c83ea44cb1d2ae
krismurillo22 <kristian_murillo@unitec.edu>
2024-08-09 13:21:21 -0600

    Interface - Avril

---
+++ src/juego_ahorcado_lab_4/JuegoAhorcadoable.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package juego_ahorcado_lab_4;
+
+/**
+ *
+ * @author User
+ */
+public interface JuegoAhorcadoable {
+    void inicializarPalabraSecreta();
+    
+    void jugar();
+}
