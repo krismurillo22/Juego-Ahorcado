3e7da7f96f8b79acbf2044fe5ecf2f1e2045eae8
krismurillo22 <kristian_murillo@unitec.edu>
2024-08-09 13:13:06 -0600

    SubidaArchivo

---
+++ src/juego_ahorcado_lab_4/Juego_Ahorcado_Lab_4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package juego_ahorcado_lab_4;
+
+/**
+ *
+ * @author User
+ */
+public class Juego_Ahorcado_Lab_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
